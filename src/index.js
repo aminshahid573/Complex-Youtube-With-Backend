6499edae55976c10c3ae9b6cce9c7365b8bf4811
Shahid Amin <96911219+aminshahid573@users.noreply.github.com>
2025-02-19 17:59:18 +0000

    work on utilities

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 dotenv.config({
-   path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
-
-
+//async function always returns a promise
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️  Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.log("MONGODB CONNECTION FAILED", error));
 
 //Nice Approach for connecting Database but it make our index.js file bigger and messy
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//this is higher order function which accepts a function
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//another way using try catch
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { asyncHandler };
+
+*/
